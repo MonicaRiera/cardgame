ec45fdff5e6b296b6881382a97f518f2f5c2a3b5
Monica <monica.riera@bts.tech>
2019-02-04 11:49:06 +0100

    Exercise corrections

--- src/main/resources/public/game.js
+++ src/main/resources/public/game.js
@@ -1,33 +1,37 @@
 // ?gameId=1
 const urlParams = new URLSearchParams(window.location.search);
 const gameId = urlParams.get('gameId');
-console.log("Game Id: " + gameId);
 
-//const game = fetch("http://localhost:8080/api/games/" + gameId);
-const gamePromise = axios.get("http://localhost:8080/api/games/" + gameId);
-
-gamePromise
-    //.then(x => x.json()) //converts the response to json
+axios.get("/api/games/" + gameId)
     .then(function (response) {
         const game = response.data;
-        // this function will be called when the data comes
-        // at this point, games contains the data that the end-point sends (the list of games)
-        const container = document.getElementById("container");
-        console.log(game);
-        let gameTitle = document.createElement("h1");
-        gameTitle.textContent = `Game ID: ${game.id}`;
-        container.appendChild(gameTitle);
-        let gameState = document.createElement("h2");
-        gameState.textContent = game.state;
-        container.appendChild(gameState);
-        let gamePlayers = document.createElement("p");
-        gamePlayers.textContent = game.toString();
-        container.appendChild(gamePlayers);
-
-        let link = document.createElement("a");
-        let linkText = document.createTextNode(`Go back to games`);
-        link.appendChild(linkText);
-        link.href = "http://localhost:8080/games.html";
-        container.appendChild(link);
-
+        displayGame(game);
+    })
+    .catch(function (error) {
+            displayError(error);
     });
+
+
+
+function displayGame(game) {
+    const container = document.getElementById("container");
+    console.log(game);
+    let gameTitle = document.createElement("h1");
+    gameTitle.textContent = `Game ID: ${game.id}`;
+    container.appendChild(gameTitle);
+    let gameState = document.createElement("h2");
+    gameState.textContent = game.state;
+    container.appendChild(gameState);
+
+
+    let gamePlayers = document.createElement("p");
+    gamePlayers.textContent = "Players: " + game.playersName;
+    container.appendChild(gamePlayers);
+}
+
+function displayError(error) {
+    let container = document.getElementById("container");
+    const p = document.createElement("p");
+    p.textContent = "The game could not be loaded: " + error;
+    container.appendChild(p);
+}
