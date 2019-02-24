5d1bb4896894f40bdf75cb7652b683b54bc0b178
Monica <monica.riera@bts.tech>
2019-02-24 11:16:34 +0100

    React components exercise

---
+++ src/main/resources/public/components/App.js
@@ -0,0 +1,7 @@
+const App = (props) => (
+  <div>
+      <Title text="Games"></Title>
+      <GamesList/>
+      <Article title="Title of the article" text="Very interesting article"></Article>
+  </div>
+);
